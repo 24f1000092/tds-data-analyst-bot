858acb7996a1aad1909b4061fee1e05e196e8877
24f1000092 <24f1000092@ds.study.iitm.ac.in>
2026-07-30 22:42:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filte
 
 TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 AIPIPE_TOKEN = os.environ["AIPIPE_TOKEN"]
-LOG_URL = "https://raw.githubusercontent.com/23f2004580/telegram-data-analyst-bot/refs/heads/main/run.jsonl" 
+LOG_URL = "https://raw.githubusercontent.com/24f1000092/tds-data-analyst-bot/refs/heads/main/run.jsonl" 
 
 
 client = OpenAI(base_url="https://aipipe.org/openai/v1", api_key=AIPIPE_TOKEN)
